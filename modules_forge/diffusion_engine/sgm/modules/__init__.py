fe7643030bcdf8526beb9ab80ca062a9f10c6d51
Haoming <hmstudy02@gmail.com>
2025-03-12 15:24:31 +0800

    3

--- modules_forge/diffusion_engine/sgm/modules/__init__.py
+++ modules_forge/diffusion_engine/sgm/modules/__init__.py
@@ -1,6 +1,1 @@
-from .encoders.modules import GeneralConditioner
-
-UNCONDITIONAL_CONFIG = {
-    "target": "sgm.modules.GeneralConditioner",
-    "params": {"emb_models": []},
-}
+from .encoders.modules import GeneralConditioner  # noqa
