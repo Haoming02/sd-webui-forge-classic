4cde28158aafb086292001df74b29078062e3913
Haoming <hmstudy02@gmail.com>
2025-03-12 10:31:09 +0800

    3

--- modules_forge/diffusion_engine/ldm/models/diffusion/ddpm.py
+++ modules_forge/diffusion_engine/ldm/models/diffusion/ddpm.py
@@ -49,23 +49,12 @@ class DDPM(torch.nn.Module):
                  parameterization="eps",
                  timesteps=1000,
                  beta_schedule="linear",
-                 monitor="val/loss",
-                 use_ema=True,
                  first_stage_key="image",
-                 image_size=256,
                  channels=3,
-                 log_every_t=100,
-                 clip_denoised=True,
                  linear_start=1e-4,
                  linear_end=2e-2,
                  cosine_s=8e-3,
                  given_betas=None,
-                 original_elbo_weight=0.0,
-                 v_posterior=0.0,
-                 l_simple_weight=1.0,
-                 scheduler_config=None,
-                 use_positional_encodings=False,
-                 make_it_fit=False,
                  *args, **kwargs
                  ):
         super().__init__()
@@ -76,27 +65,8 @@ class DDPM(torch.nn.Module):
         self.cond_stage_model: torch.nn.Module = None
         self.first_stage_model: torch.nn.Module = None
 
-        self.clip_denoised = clip_denoised
-        self.log_every_t = log_every_t
         self.first_stage_key = first_stage_key
-        self.image_size = image_size
         self.channels = channels
-        self.use_positional_encodings = use_positional_encodings
-        self.v_posterior = v_posterior
-        self.original_elbo_weight = original_elbo_weight
-        self.l_simple_weight = l_simple_weight
-        self.make_it_fit = make_it_fit
-
-        self.use_ema = use_ema
-        if self.use_ema:
-            self.model_ema = LitEma(self.model)
-
-        self.use_scheduler = scheduler_config is not None
-        if self.use_scheduler:
-            self.scheduler_config = scheduler_config
-
-        if monitor is not None:
-            self.monitor = monitor
 
         self.register_schedule(given_betas=given_betas, beta_schedule=beta_schedule, timesteps=timesteps,
                                linear_start=linear_start, linear_end=linear_end, cosine_s=cosine_s)
