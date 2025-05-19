f50236c89c74a0003bc5b908c323258e8a3b999e
Logan Booker <loganbooker@gmail.com>
2025-05-19 14:51:46 +1000

    Ignore LoRAs if prompt weight is zero.

--- extensions-builtin/Lora/extra_networks_lora.py
+++ extensions-builtin/Lora/extra_networks_lora.py
@@ -23,10 +23,10 @@ class ExtraNetworkLora(extra_networks.ExtraNetwork):
         te_multipliers = []
         unet_multipliers = []
         dyn_dims = []
+        skipped_loras = []
+
         for params in params_list:
             assert params.items
-
-            names.append(params.positional[0])
 
             te_multiplier = float(params.positional[1]) if len(params.positional) > 1 else 1.0
             te_multiplier = float(params.named.get("te", te_multiplier))
@@ -37,9 +37,16 @@ class ExtraNetworkLora(extra_networks.ExtraNetwork):
             dyn_dim = int(params.positional[3]) if len(params.positional) > 3 else None
             dyn_dim = int(params.named["dyn"]) if "dyn" in params.named else dyn_dim
 
-            te_multipliers.append(te_multiplier)
-            unet_multipliers.append(unet_multiplier)
-            dyn_dims.append(dyn_dim)
+            if te_multiplier == 0.0 and unet_multiplier == 0.0:
+                skipped_loras.append(params.positional[0]);
+            else:
+                names.append(params.positional[0])
+                te_multipliers.append(te_multiplier)
+                unet_multipliers.append(unet_multiplier)
+                dyn_dims.append(dyn_dim)
+
+        if skipped_loras:
+            print(f"\n[LORA] Ignoring: {', '.join(skipped_loras)}. Multipliers are 0.")
 
         networks.load_networks(names, te_multipliers, unet_multipliers, dyn_dims)
 
