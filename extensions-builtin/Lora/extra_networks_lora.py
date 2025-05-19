5c0b23308020d945e2d1fc4b4d567a31289b76a2
Haoming <hmstudy02@gmail.com>
2025-05-19 13:49:04 +0800

    remove logging

--- extensions-builtin/Lora/extra_networks_lora.py
+++ extensions-builtin/Lora/extra_networks_lora.py
@@ -23,8 +23,6 @@ class ExtraNetworkLora(extra_networks.ExtraNetwork):
         te_multipliers = []
         unet_multipliers = []
         dyn_dims = []
-        skipped_loras = []
-
         for params in params_list:
             assert params.items
 
@@ -34,19 +32,16 @@ class ExtraNetworkLora(extra_networks.ExtraNetwork):
             unet_multiplier = float(params.positional[2]) if len(params.positional) > 2 else te_multiplier
             unet_multiplier = float(params.named.get("unet", unet_multiplier))
 
+            if te_multiplier == 0.0 and unet_multiplier == 0.0:
+                continue
+
             dyn_dim = int(params.positional[3]) if len(params.positional) > 3 else None
             dyn_dim = int(params.named["dyn"]) if "dyn" in params.named else dyn_dim
 
-            if te_multiplier == 0.0 and unet_multiplier == 0.0:
-                skipped_loras.append(params.positional[0]);
-            else:
-                names.append(params.positional[0])
-                te_multipliers.append(te_multiplier)
-                unet_multipliers.append(unet_multiplier)
-                dyn_dims.append(dyn_dim)
-
-        if skipped_loras:
-            print(f"\n[LORA] Ignoring: {', '.join(skipped_loras)}. Multipliers are 0.")
+            names.append(params.positional[0])
+            te_multipliers.append(te_multiplier)
+            unet_multipliers.append(unet_multiplier)
+            dyn_dims.append(dyn_dim)
 
         networks.load_networks(names, te_multipliers, unet_multipliers, dyn_dims)
 
