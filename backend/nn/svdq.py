c727967452548d50f60f16584799b3c8bb92178d
Haoming <hmstudy02@gmail.com>
2025-10-16 18:29:11 +0800

    ref

--- backend/nn/svdq.py
+++ backend/nn/svdq.py
@@ -602,6 +602,9 @@ class NunchakuQwenImageTransformer2DModel(NunchakuModelMixin, QwenImageTransform
         hidden_states, img_ids, orig_shape = self.process_img(x)
         num_embeds = hidden_states.shape[1]
 
+        if dynamic_args.get("ref_latents", None) is not None:
+            ref_latents = dynamic_args["ref_latents"]
+
         if ref_latents is not None:
             h = 0
             w = 0
@@ -623,7 +626,7 @@ class NunchakuQwenImageTransformer2DModel(NunchakuModelMixin, QwenImageTransform
                     h = max(h, ref.shape[-2] + h_offset)
                     w = max(w, ref.shape[-1] + w_offset)
 
-                kontext, kontext_ids, _ = self.process_img(ref, index=index, h_offset=h_offset, w_offset=w_offset)
+                kontext, kontext_ids, _ = self.process_img(ref.to(x), index=index, h_offset=h_offset, w_offset=w_offset)
                 hidden_states = torch.cat([hidden_states, kontext], dim=1)
                 img_ids = torch.cat([img_ids, kontext_ids], dim=1)
 
