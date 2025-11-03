f74e6598a417d55e4f292c230e6b8204b0dfc820
Haoming <hmstudy02@gmail.com>
2025-11-03 16:47:03 +0800

    openpose

--- extensions-builtin/sd_forge_controlnet/javascript/openpose_editor.js
+++ extensions-builtin/sd_forge_controlnet/javascript/openpose_editor.js
@@ -1,54 +1,37 @@
 (function () {
-    const cnetOpenposeEditorRegisteredElements = new Set();
-    function loadOpenposeEditor() {
-        // Simulate an `input` DOM event for Gradio Textbox component. Needed after you edit its contents in javascript, otherwise your edits
-        // will only visible on web page and not sent to python.
-        function updateInput(target) {
-            let e = new Event("input", { bubbles: true })
-            Object.defineProperty(e, "target", { value: target })
-            target.dispatchEvent(e);
-        }
+    const AllCnetTabs = new Set();
 
-        const tabs = gradioApp().querySelectorAll('#controlnet .tabitem');
-        tabs.forEach(tab => {
-            if (cnetOpenposeEditorRegisteredElements.has(tab)) return;
-            cnetOpenposeEditorRegisteredElements.add(tab);
+    function setupOpenPoseEditor() {
+        const tabs = document.querySelectorAll("#controlnet .tabitem");
 
-            const generatedImageGroup = tab.querySelector('.cnet-generated-image-group');
-            /*
-            * Writes the pose data URL to an link element on input image group.
-            * Click a hidden button to trigger a backend rendering of the pose JSON.
-            *
-            * The backend should:
-            * - Set the rendered pose image as preprocessor generated image.
-            */
+        for (const tab of tabs) {
+            if (AllCnetTabs.has(tab)) continue;
+            AllCnetTabs.add(tab);
+
+            const generatedImageGroup = tab.querySelector(".cnet-generated-image-group");
+            const allowPreviewCheckbox = tab.querySelector(".cnet-allow-preview input");
+
+            const downloadLink = generatedImageGroup.querySelector(".cnet-download-pose a");
+            const poseTextbox = generatedImageGroup.querySelector(".cnet-pose-json textarea");
+            const renderButton = generatedImageGroup.querySelector(".cnet-render-pose");
+
             function updatePreviewPose(poseURL) {
-                const downloadLink = generatedImageGroup.querySelector('.cnet-download-pose a');
-                const renderButton = generatedImageGroup.querySelector('.cnet-render-pose');
-                const poseTextbox = generatedImageGroup.querySelector('.cnet-pose-json textarea');
-                const allowPreviewCheckbox = tab.querySelector('.cnet-allow-preview input');
+                if (!allowPreviewCheckbox.checked) allowPreviewCheckbox.click();
 
-                if (!allowPreviewCheckbox.checked)
-                    allowPreviewCheckbox.click();
-
-                // Only set href when download link exists and needs an update. `downloadLink`
-                // can be null when user closes preview and click `Upload JSON` button again.
-                // https://github.com/Mikubill/sd-webui-controlnet/issues/2308
-                if (downloadLink !== null)
-                    downloadLink.href = poseURL;
+                if (downloadLink != null) downloadLink.href = poseURL;
 
                 poseTextbox.value = poseURL;
                 updateInput(poseTextbox);
+
                 renderButton.click();
             }
 
-            const inputImageGroup = tab.querySelector('.cnet-input-image-group');
-            const uploadButton = inputImageGroup.querySelector('.cnet-upload-pose input');
-            // Updates preview image when JSON file is uploaded.
-            uploadButton.addEventListener('change', (event) => {
+            const inputImageGroup = tab.querySelector(".cnet-input-image-group");
+            const uploadButton = inputImageGroup.querySelector(".cnet-upload-pose input");
+
+            uploadButton.addEventListener("change", (event) => {
                 const file = event.target.files[0];
-                if (!file)
-                    return;
+                if (!file) return;
 
                 const reader = new FileReader();
                 reader.onload = function (e) {
@@ -57,11 +40,11 @@
                     updatePreviewPose(poseURL);
                 };
                 reader.readAsText(file);
-                // Reset the file input value so that uploading the same file still triggers callback.
-                event.target.value = '';
+
+                event.target.value = "";
             });
-        });
+        }
     }
 
-    onUiLoaded(loadOpenposeEditor);
+    onUiLoaded(setupOpenPoseEditor);
 })();
