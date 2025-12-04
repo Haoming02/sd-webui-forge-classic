131bf12415a6763a67e92569b4f64d74b069196c
Nandaka <nchek2000@gmail.com>
2025-12-04 20:06:28 +0800

    add option to append filename (without extension) to the prompt

--- modules/img2img.py
+++ modules/img2img.py
@@ -111,7 +111,14 @@ def process_batch(p, input, output_dir, inpaint_mask_dir, args, to_scale=False, 
             except Exception:
                 parsed_parameters = {}
 
-            p.prompt = prompt + (" " + parsed_parameters["Prompt"] if "Prompt" in parsed_parameters else "")
+            filename = ""
+            if "Filename" in png_info_props:
+                filename = Path(image).stem
+                filename = filename.replace("(", "\\(").replace(")", "\\)").replace("_", " ")
+                filename = f"(, {filename}:1.5). "
+                print(f"Appending {filename} to the prompt.")
+
+            p.prompt = prompt + (" " + parsed_parameters["Prompt"] if "Prompt" in parsed_parameters else "") + filename
             p.negative_prompt = negative_prompt + (" " + parsed_parameters["Negative prompt"] if "Negative prompt" in parsed_parameters else "")
             p.seed = int(parsed_parameters.get("Seed", seed))
             p.cfg_scale = float(parsed_parameters.get("CFG scale", cfg_scale))
