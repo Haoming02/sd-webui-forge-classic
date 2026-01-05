caee71050b4f37dd7f1043bfda51054b54e79357
Haoming <hmstudy02@gmail.com>
2026-01-05 11:53:55 +0800

    device

--- backend/memory_management.py
+++ backend/memory_management.py
@@ -694,32 +694,32 @@ def dtype_size(dtype: torch.dtype) -> int:
 
 
 def unet_offload_device():
-    if vram_state == VRAMState.HIGH_VRAM:
+    if vram_state is VRAMState.HIGH_VRAM:
         return get_torch_device()
     else:
         return torch.device("cpu")
 
 
-def unet_inital_load_device(parameters, dtype):
+def unet_initial_load_device(parameters: int, dtype: torch.dtype) -> torch.device:
     torch_dev = get_torch_device()
-    if vram_state == VRAMState.HIGH_VRAM or vram_state == VRAMState.SHARED:
+    if vram_state in (VRAMState.HIGH_VRAM, VRAMState.SHARED):
         return torch_dev
 
     cpu_dev = torch.device("cpu")
-    if DISABLE_SMART_MEMORY or vram_state == VRAMState.NO_VRAM:
+    if DISABLE_SMART_MEMORY or vram_state is VRAMState.NO_VRAM:
         return cpu_dev
 
     model_size = dtype_size(dtype) * parameters
-
     mem_dev = get_free_memory(torch_dev)
     mem_cpu = get_free_memory(cpu_dev)
+
     if mem_dev > mem_cpu and model_size < mem_dev:
         return torch_dev
     else:
         return cpu_dev
 
 
-def maximum_vram_for_weights(device=None):
+def maximum_vram_for_weights(device: torch.device = None) -> float:
     return get_total_memory(device) * 0.88 - minimum_inference_memory()
 
 
