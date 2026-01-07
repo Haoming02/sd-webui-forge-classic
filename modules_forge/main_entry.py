bf4d4d1270acd06877cef7c8cdd5cc70457a927f
Haoming <hmstudy02@gmail.com>
2026-01-07 11:45:04 +0800

    paths

--- modules_forge/main_entry.py
+++ modules_forge/main_entry.py
@@ -1,10 +1,10 @@
-import os
+import os.path
 
 import gradio as gr
 import torch
 from gradio.context import Context
 
-from backend import memory_management, operations, stream
+from backend import memory_management, operations
 from backend.args import dynamic_args
 from modules import infotext_utils, paths, processing, sd_models, shared, shared_items, ui_common
 
@@ -34,8 +34,8 @@ if operations.bnb_available:
         }
     )
 
-module_list = {}
 
+module_list: dict[str, os.PathLike] = {}
 
 
 def bind_to_opts(comp: gr.components.Component, k: str, save: bool = False, callback=None):
@@ -64,18 +64,18 @@ def make_checkpoint_manager_ui():
 
     ui_vae = gr.Dropdown(label="VAE / Text Encoder", value=None, choices=None, multiselect=True)
 
-    def gr_refresh_models():
+    def refresh_model_list():
         ckpt_list, vae_list = refresh_models()
         return gr.update(choices=ckpt_list), gr.update(choices=vae_list)
 
     refresh_button = ui_common.ToolButton(value=ui_common.refresh_symbol, elem_id="forge_refresh_checkpoint", tooltip="Refresh")
-    refresh_button.click(fn=gr_refresh_models, outputs=[ui_checkpoint, ui_vae], queue=False)
+    refresh_button.click(fn=refresh_model_list, outputs=[ui_checkpoint, ui_vae], queue=False)
 
-    def gr_refresh_on_load():
+    def model_on_load():
         ckpt_list, vae_list = refresh_models()
         return [gr.update(value=shared.opts.sd_model_checkpoint, choices=ckpt_list), gr.update(value=[os.path.basename(x) for x in shared.opts.forge_additional_modules], choices=vae_list)]
 
-    Context.root_block.load(fn=gr_refresh_on_load, outputs=[ui_checkpoint, ui_vae], show_progress=False, queue=False)
+    Context.root_block.load(fn=model_on_load, outputs=[ui_checkpoint, ui_vae], show_progress=False, queue=False)
 
     ui_forge_unet_dtype = gr.Dropdown(label="Diffusion in Low Bits", value=lambda: shared.opts.forge_unet_storage_dtype, choices=list(forge_unet_storage_dtype_options.keys()))
     bind_to_opts(ui_forge_unet_dtype, "forge_unet_storage_dtype", save=True, callback=refresh_model_loading_parameters)
@@ -84,7 +84,7 @@ def make_checkpoint_manager_ui():
     ui_vae.change(modules_change, inputs=[ui_vae, ui_forge_preset], queue=False, show_progress=False)
 
 
-def find_files_with_extensions(base_path, extensions):
+def find_files_with_extensions(base_path: os.PathLike, extensions: list[str]) -> dict[str, os.PathLike]:
     found_files = {}
     for root, _, files in os.walk(base_path):
         for file in files:
@@ -94,9 +94,7 @@ def find_files_with_extensions(base_path, extensions):
     return found_files
 
 
-def refresh_models():
-    global module_list
-
+def refresh_models() -> tuple[list[os.PathLike], list[os.PathLike]]:
     shared_items.refresh_checkpoints()
     ckpt_list = shared_items.list_checkpoint_tiles(shared.opts.sd_checkpoint_dropdown_use_short)
 
@@ -104,7 +102,7 @@ def refresh_models():
 
     module_list.clear()
 
-    module_paths: set[str] = {
+    module_paths: set[os.PathLike] = {
         os.path.abspath(os.path.join(paths.models_path, "VAE")),
         os.path.abspath(os.path.join(paths.models_path, "text_encoder")),
         *shared.cmd_opts.vae_dirs,
@@ -164,9 +162,10 @@ def modules_change(module_values: list, preset: str, save=True, refresh=True) ->
         module_name = os.path.basename(v)  # If the input is a filepath, extract the file name
         if module_name in module_list:
             modules.append(module_list[module_name])
+    modules.sort()
 
     # skip further processing if value unchanged
-    if sorted(modules) == sorted(shared.opts.data.get("forge_additional_modules", [])):
+    if modules == shared.opts.data.get("forge_additional_modules", []):
         return False
 
     shared.opts.set("forge_additional_modules", modules)
@@ -180,7 +179,7 @@ def modules_change(module_values: list, preset: str, save=True, refresh=True) ->
     return True
 
 
-def get_a1111_ui_component(tab, label):
+def get_a1111_ui_component(tab: str, label: str) -> gr.components.Component:
     fields = infotext_utils.paste_fields[tab]["fields"]
     for f in fields:
         if f.label == label or f.api == label:
@@ -241,15 +240,11 @@ def on_preset_change(preset: str):
     shared.opts.set("forge_preset", preset)
     shared.opts.save(shared.config_filename)
 
-    model_mem = getattr(shared.opts, f"{preset}_gpu_mb", total_vram - 1024)
-    if model_mem < 0 or model_mem > total_vram:
-        model_mem = total_vram - 1024
-
-    distilled = preset in ("flux", "lumina", "wan")
-    d_label = "Distilled CFG Scale" if preset == "flux" else "Shift"
-    batch_args = {"minimum": 1, "maximum": 97, "step": 16, "label": "Frames", "value": 1} if preset == "wan" else {"minimum": 1, "maximum": 8, "step": 1, "label": "Batch size", "value": 1}
-
     additional_modules = [os.path.basename(x) for x in getattr(shared.opts, f"forge_additional_modules_{preset}", [])]
+
+    extra_slider = preset in ("flux", "lumina", "wan")
+    distill_label = "Distilled CFG Scale" if preset == "flux" else "Shift"
+    batch_args = {"minimum": 1, "maximum": 97, "step": 16, "label": "Frames", "value": 1} if preset == "wan" else {"minimum": 1, "maximum": 8, "step": 1, "label": "Batch size", "value": 1}
 
     return [
         gr.update(value=getattr(shared.opts, f"forge_checkpoint_{preset}", shared.opts.sd_model_checkpoint)),  # ui_checkpoint
@@ -261,14 +256,14 @@ def on_preset_change(preset: str):
         gr.update(value=getattr(shared.opts, f"{preset}_i2i_height", 768)),  # ui_img2img_height
         gr.update(value=getattr(shared.opts, f"{preset}_t2i_cfg", 1.0)),  # ui_txt2img_cfg
         gr.update(value=getattr(shared.opts, f"{preset}_i2i_cfg", 1.0)),  # ui_img2img_cfg
-        gr.update(visible=distilled, label=d_label, value=getattr(shared.opts, f"{preset}_t2i_d_cfg", 3.0)),  # ui_txt2img_distilled_cfg
-        gr.update(visible=distilled, label=d_label, value=getattr(shared.opts, f"{preset}_i2i_d_cfg", 3.0)),  # ui_img2img_distilled_cfg
+        gr.update(visible=extra_slider, label=distill_label, value=getattr(shared.opts, f"{preset}_t2i_d_cfg", 3.0)),  # ui_txt2img_distilled_cfg
+        gr.update(visible=extra_slider, label=distill_label, value=getattr(shared.opts, f"{preset}_i2i_d_cfg", 3.0)),  # ui_img2img_distilled_cfg
         gr.update(value=getattr(shared.opts, f"{preset}_t2i_sampler", "Euler")),  # ui_txt2img_sampler
         gr.update(value=getattr(shared.opts, f"{preset}_i2i_sampler", "Euler")),  # ui_img2img_sampler
         gr.update(value=getattr(shared.opts, f"{preset}_t2i_scheduler", "Simple")),  # ui_txt2img_scheduler
         gr.update(value=getattr(shared.opts, f"{preset}_i2i_scheduler", "Simple")),  # ui_img2img_scheduler
         gr.update(value=getattr(shared.opts, f"{preset}_t2i_hr_cfg", 1.0)),  # ui_txt2img_hr_cfg
-        gr.update(visible=distilled, label=d_label, value=getattr(shared.opts, f"{preset}_t2i_hr_d_cfg", 3.0)),  # ui_txt2img_hr_distilled_cfg
+        gr.update(visible=extra_slider, label=distill_label, value=getattr(shared.opts, f"{preset}_t2i_hr_d_cfg", 3.0)),  # ui_txt2img_hr_distilled_cfg
         gr.update(**batch_args),  # ui_txt2img_batch_size
         gr.update(**batch_args),  # ui_img2img_batch_size
     ]
