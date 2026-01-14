5674e56f15e815bf018520754c609137fc290863
Haoming <hmstudy02@gmail.com>
2026-01-14 11:30:24 +0800

    logging

--- backend/logging.py
+++ backend/logging.py
@@ -1,47 +1,65 @@
-import copy
 import logging
-import os
-import sys
-import textwrap
-from typing import Final
+from typing import Iterable, Optional
+
+from rich.console import ConsoleRenderable, RenderableType
+from rich.containers import Renderables
+from rich.logging import RichHandler
+from rich.table import Table
+from rich.text import Text
 
 from backend.args import args
 
-MAX_CHAR: int = os.get_terminal_size().columns
 
+class ForgeRender:
 
-class ForgeFormatter(logging.Formatter):
-    RESET: Final[str] = "\033[0m"
+    def __call__(self, console, renderables: Iterable["ConsoleRenderable"], log_time=None, time_format=None, level: str = "", path: Optional[str] = None, line_no=None, link_path=None) -> Table:
+        output = Table.grid(padding=(0, 1))
+        output.expand = True
 
-    COLORS: Final[dict[str, str]] = {
-        "DEBUG": "\033[0;90m",  # Gray
-        "INFO": "\033[0;96m",  # Cyan
-        "WARNING": "\033[0;93m",  # Yellow
-        "ERROR": "\033[0;91m",  # Red
-        "CRITICAL": "\033[0;37;41m",  # White on Red
-    }
+        output.add_column(ratio=1, style="log.message", overflow="fold")
+        if path:
+            output.add_column(style="log.path")
+            output.add_column()
+        output.add_column(style="log.level")
 
-    @classmethod
-    def _message(cls, color: str, msg: str, file: str) -> str:
-        file_len: int = len(file)
-        msg: str = textwrap.fill(msg, width=MAX_CHAR - file_len - 4)
-        line_len: str = len(msg.rsplit("\n", 1)[-1])
-        gap: int = MAX_CHAR - line_len - file_len
-        return f"{color}{msg}{cls.RESET}{' ' * gap}{cls.COLORS['DEBUG']}{file}{cls.RESET}"
+        row: list["RenderableType"] = [Renderables(renderables)]
 
-    def format(self, record):
-        new_record = copy.copy(record)
-        color = ForgeFormatter.COLORS[record.levelname]
-        new_record.msg = ForgeFormatter._message(color, record.msg, record.name)
-        return super().format(new_record)
+        if path:
+            path_text = Text()
+            path_text.append(path)
+
+            row.append(path_text)
+            row.append("::")
+
+        row.append(level)
+        output.add_row(*row)
+
+        return output
 
 
 def setup_logger(logger: logging.Logger):
     logger.propagate = False
 
     if not logger.handlers:
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(ForgeFormatter("%(message)s"))
+        handler = RichHandler(
+            keywords=[
+                "cpu",
+                "cuda",
+                "rocm",
+                "torch",
+                "float16",
+                "float32",
+                "bfloat16",
+                "float8_e4m3fn",
+                "float8_e5m2",
+                "gguf",
+                "nf4",
+                "fp4",
+                "int4",
+            ]
+        )
+        handler._log_render = ForgeRender()
+        handler.setFormatter(logging.Formatter("%(message)s"))
         logger.addHandler(handler)
 
     logger.setLevel(args.loglevel or "INFO")
