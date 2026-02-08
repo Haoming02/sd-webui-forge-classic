3b9fbdeb1af5d5ee98039ba62459487e02c7eeb2
Haoming <hmstudy02@gmail.com>
2026-02-09 01:19:11 +0800

    tiled

--- extensions-builtin/sd_forge_multidiffusion/scripts/forge_multidiffusion.py
+++ extensions-builtin/sd_forge_multidiffusion/scripts/forge_multidiffusion.py
@@ -14,8 +14,7 @@ class MultiDiffusionForForge(scripts.Script):
         return "MultiDiffusion Integrated"
 
     def show(self, is_img2img):
-        # make this extension visible in both txt2img and img2img tab.
-        return scripts.AlwaysVisible
+        return scripts.AlwaysVisible if is_img2img else None
 
     def ui(self, *args, **kwargs):
         with gr.Accordion(open=False, label=self.title()):
