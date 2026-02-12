dc5d998949cd4e9caaa1543d4a7c693bc2156b10
Haoming <hmstudy02@gmail.com>
2026-02-12 17:25:00 +0800

    multi

--- extensions-builtin/sd_forge_multidiffusion/lib_multidiffusion/tiled_diffusion.py
+++ extensions-builtin/sd_forge_multidiffusion/lib_multidiffusion/tiled_diffusion.py
@@ -606,44 +606,24 @@ class MixtureOfDiffusers(AbstractDiffusion):
 MAX_RESOLUTION = 8192
 
 
-class TiledDiffusion():
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required": {"model": ("MODEL",),
-                             "method": (["MultiDiffusion", "Mixture of Diffusers"], {"default": "Mixture of Diffusers"}),
-                             # "tile_width": ("INT", {"default": 96, "min": 16, "max": 256, "step": 16}),
-                             "tile_width": ("INT", {"default": 96 * opt_f, "min": 16, "max": MAX_RESOLUTION, "step": 16}),
-                             # "tile_height": ("INT", {"default": 96, "min": 16, "max": 256, "step": 16}),
-                             "tile_height": ("INT", {"default": 96 * opt_f, "min": 16, "max": MAX_RESOLUTION, "step": 16}),
-                             "tile_overlap": ("INT", {"default": 8 * opt_f, "min": 0, "max": 256 * opt_f, "step": 4 * opt_f}),
-                             "tile_batch_size": ("INT", {"default": 4, "min": 1, "max": MAX_RESOLUTION, "step": 1}),
-                             }}
+class TiledDiffusion:
 
-    RETURN_TYPES = ("MODEL",)
-    FUNCTION = "apply"
-    CATEGORY = "_for_testing"
-
-    def apply(self, model: ModelPatcher, method, tile_width, tile_height, tile_overlap, tile_batch_size):
-        if method == "Mixture of Diffusers":
-            implement = MixtureOfDiffusers()
-        else:
-            implement = MultiDiffusion()
-
-        # if noise_inversion:
-        #     get_cache_callback = self.noise_inverse_get_cache
-        #     set_cache_callback = None # lambda x0, xt, prompts: self.noise_inverse_set_cache(p, x0, xt, prompts, steps, retouch)
-        #     implement.init_noise_inverse(steps, retouch, get_cache_callback, set_cache_callback, renoise_strength, renoise_kernel_size)
+    @staticmethod
+    def apply(model: ModelPatcher, method: str, tile_width: int, tile_height: int, tile_overlap: int, tile_batch_size: int):
+        match method:
+            case "MultiDiffusion":
+                implement = MultiDiffusion()
+            case "Mixture of Diffusers":
+                implement = MixtureOfDiffusers()
+            case _:
+                raise SystemError
 
         implement.tile_width = tile_width // opt_f
         implement.tile_height = tile_height // opt_f
         implement.tile_overlap = tile_overlap // opt_f
         implement.tile_batch_size = tile_batch_size
-        # implement.init_grid_bbox(tile_width, tile_height, tile_overlap, tile_batch_size)
-        # # init everything done, perform sanity check & pre-computations
-        # implement.init_done()
-        # hijack the behaviours
-        # implement.hook()
+
         model = model.clone()
         model.set_model_unet_function_wrapper(implement)
-        model.model_options['tiled_diffusion'] = True
-        return (model,)
+        model.model_options["tiled_diffusion"] = True
+        return model
