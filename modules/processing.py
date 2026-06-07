1a369d126ee4876dca175bd696a094dfed2318ec
Haoming <hmstudy02@gmail.com>
2026-06-08 00:03:47 +0800

    dim

--- modules/processing.py
+++ modules/processing.py
@@ -944,8 +944,12 @@ def process_images_inner(p: StableDiffusionProcessing) -> Processed:
             p.seeds = p.all_seeds[n * p.batch_size : (n + 1) * p.batch_size]
             p.subseeds = p.all_subseeds[n * p.batch_size : (n + 1) * p.batch_size]
 
-            latent_channels = shared.sd_model.forge_objects.vae.latent_channels
-            _shape = (latent_channels, _times, p.height // opt_f, p.width // opt_f) if shared.sd_model.is_wan else (latent_channels, p.height // opt_f, p.width // opt_f)
+            if args.dynamic_args.pid:
+                _shape = (3, p.height, p.width)
+            else:
+                latent_channels = shared.sd_model.forge_objects.vae.latent_channels
+                _shape = (latent_channels, _times, p.height // opt_f, p.width // opt_f) if shared.sd_model.is_wan else (latent_channels, p.height // opt_f, p.width // opt_f)
+
             p.rng = rng.ImageRNG(_shape, p.seeds, subseeds=p.subseeds, subseed_strength=p.subseed_strength, seed_resize_from_h=p.seed_resize_from_h, seed_resize_from_w=p.seed_resize_from_w)
 
             if p.scripts is not None:
