c85d45b495b7ad22d607460c21445383a636ebab
Haoming <hmstudy02@gmail.com>
2026-09-26 15:51:41 +0800

    gemma

--- backend/text_processing/gemma_engine.py
+++ backend/text_processing/gemma_engine.py
@@ -8,157 +8,53 @@ if TYPE_CHECKING:
 
 import torch
 
-from backend import memory_management
 from backend.args import dynamic_args
-from backend.text_processing import emphasis, parsing
+from backend.text_processing import emphasis
 from modules.shared import opts
 
-
-class PromptChunk:
-    def __init__(self):
-        self.tokens = []
-        self.multipliers = []
+from ._comfy import INF, SDClipModel, SDTokenizer
 
 
 class GemmaTextProcessingEngine:
     def __init__(self, text_encoder, tokenizer):
-        self.emphasis = emphasis.get_current_option(opts.emphasis)()
+        self.text_encoder = SDClipModel(text_encoder, layer="hidden", layer_idx=-2, special_tokens={"start": 2, "pad": 0}, layer_norm_hidden_state=False, enable_attention_masks=True, return_attention_masks=True)
+        self.tokenizer = SDTokenizer(tokenizer, pad_with_end=False, has_end_token=False, pad_to_max_length=False, max_length=INF, min_length=1)
 
-        self.text_encoder = text_encoder
-        self.tokenizer = tokenizer
+    @property
+    def emphasis(self) -> "emphasis.Emphasis":
+        return emphasis.get_current_option(opts.emphasis)()
 
-        self.id_start = 2
-        self.id_pad = 0
-
-        self.intermediate_output = -2
-        self.layer_norm_hidden_state = False
-
-    def tokenize(self, texts):
-        tokenized = self.tokenizer(texts, truncation=False, add_special_tokens=False)["input_ids"]
-        return tokenized
-
-    def tokenize_line(self, line):
-        parsed = parsing.parse_prompt_attention(line, self.emphasis.name)
-
-        tokenized = self.tokenize([text for text, _ in parsed])
-
-        chunks = []
-        chunk = PromptChunk()
-
-        def next_chunk():
-            nonlocal chunk
-
-            chunk.tokens = [self.id_start] + chunk.tokens
-            chunk.multipliers = [1.0] + chunk.multipliers
-
-            chunks.append(chunk)
-            chunk = PromptChunk()
-
-        for tokens, (text, weight) in zip(tokenized, parsed):
-            if text == "BREAK" and weight == -1:
-                next_chunk()
-                continue
-
-            position = 0
-            while position < len(tokens):
-                token = tokens[position]
-                chunk.tokens.append(token)
-                chunk.multipliers.append(weight)
-                position += 1
-
-        if chunk.tokens or not chunks:
-            next_chunk()
-
-        return chunks
+    def tokenize(self, texts: list[str]) -> tuple[list[int], list[int]]:
+        return self.tokenizer.tokenizer(texts)["input_ids"]
 
     @staticmethod
-    def process_template(text: str, negative: bool) -> str:
+    def process_template(text: str, is_negative: bool) -> str:
         if "<Prompt Start>" in text:
             return text
 
-        from modules.shared import opts
+        return "\n".join([opts.neta_template_negative if is_negative else opts.neta_template_positive, text])
 
-        if negative:
-            return "\n".join([opts.neta_template_negative, text])
-        else:
-            return "\n".join([opts.neta_template_positive, text])
-
-    def __call__(self, texts: "SdConditioning"):
-        self.emphasis = emphasis.get_current_option(opts.emphasis)()
-        if any(emphasis.uses_emphasis(x) for x in texts):
+    def __call__(self, texts: "SdConditioning") -> torch.Tensor:
+        if any(emphasis.uses_emphasis(text) for text in texts) and self.emphasis.name in ("None", "Ignore"):
             dynamic_args.last_extra_generation_params["Emphasis"] = self.emphasis.name
 
         zs = []
-        cache = {}
+        cache: dict[str, torch.Tensor] = {}
 
         for line in texts:
             line = self.process_template(line, texts.is_negative_prompt)
 
             if line in cache:
-                line_z_values = cache[line]
+                cond = cache[line]
             else:
-                chunks = self.tokenize_line(line)
-                line_z_values = []
+                chunk = self.tokenizer.tokenize_with_weights(line, disable_weights=self.emphasis.name == "None")
 
-                for chunk in chunks:
-                    tokens = chunk.tokens
-                    multipliers = chunk.multipliers
+                if self.emphasis.name == "Ignore":
+                    chunk = [[(x[0], 1.0) for x in inner] for inner in chunk]
 
-                    z = self.process_tokens([tokens], [multipliers])[0]
-                    line_z_values.append(z)
-                cache[line] = line_z_values
+                cond = self.text_encoder.encode_token_weights(chunk)[0]
+                cache[line] = cond
 
-            zs.extend(line_z_values)
+            zs.extend(cond)
 
         return zs
-
-    def process_embeds(self, batch_tokens):
-        device = memory_management.text_encoder_device()
-
-        embeds_out = []
-        attention_masks = []
-        num_tokens = []
-
-        for tokens in batch_tokens:
-            attention_mask = []
-            tokens_temp = []
-            eos = False
-            index = 0
-
-            for t in tokens:
-                token = int(t)
-                attention_mask.append(0 if eos else 1)
-                tokens_temp += [token]
-                if not eos and token == self.id_pad:
-                    eos = True
-                index += 1
-
-            tokens_embed = torch.tensor([tokens_temp], device=device, dtype=torch.long)
-            tokens_embed = self.text_encoder.get_input_embeddings()(tokens_embed)
-
-            index = 0
-
-            embeds_out.append(tokens_embed)
-            attention_masks.append(attention_mask)
-            num_tokens.append(sum(attention_mask))
-
-        return torch.cat(embeds_out), torch.tensor(attention_masks, device=device, dtype=torch.long), num_tokens
-
-    def process_tokens(self, batch_tokens, batch_multipliers):
-        embeds, mask, count = self.process_embeds(batch_tokens)
-
-        self.emphasis.tokens = batch_tokens
-        self.emphasis.multipliers = torch.asarray(batch_multipliers).to(embeds)
-        self.emphasis.z = embeds
-        self.emphasis.after_transformers()
-        embeds = self.emphasis.z
-
-        _, z = self.text_encoder(
-            None,
-            attention_mask=mask,
-            embeds=embeds,
-            num_tokens=count,
-            intermediate_output=self.intermediate_output,
-            final_layer_norm_intermediate=self.layer_norm_hidden_state,
-        )
-        return z
