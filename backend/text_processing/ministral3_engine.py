7149a0664bfb47381453c5dad978ac3983adfafd
Haoming <hmstudy02@gmail.com>
2026-09-26 20:00:30 +0800

    ministral

--- backend/text_processing/ministral3_engine.py
+++ backend/text_processing/ministral3_engine.py
@@ -3,135 +3,55 @@
 
 import torch
 
-from backend import memory_management
 from backend.args import dynamic_args
-from backend.text_processing import emphasis, parsing
+from backend.text_processing import emphasis
+
+from ._comfy import INF, SDClipModel, SDTokenizer
 
 
-class PromptChunk:
-    def __init__(self):
-        self.tokens = []
-        self.multipliers = []
+class Wrap:
+
+    def __init__(self, tokenizer):
+        self.model = tokenizer
+
+    def __call__(self, text: str, *args, **kwargs):
+        z: torch.Tensor = self.model(text=text)["input_ids"]
+        return {"input_ids": z.squeeze(0).tolist()}
+
+    def __getattr__(self, name):
+        return getattr(self.model, name)
 
 
 class Ministral3TextProcessingEngine:
     def __init__(self, text_encoder, tokenizer):
-        self.emphasis = emphasis.EmphasisNone()
+        self.text_encoder = SDClipModel(text_encoder, layer="hidden", layer_idx=-2, special_tokens={"start": 1, "pad": 0}, layer_norm_hidden_state=False, enable_attention_masks=True, return_attention_masks=True)
+        self.tokenizer = SDTokenizer(Wrap(tokenizer), pad_with_end=False, has_end_token=False, pad_to_max_length=False, pad_token=11, start_token=1, max_length=INF, min_length=1, pad_left=True, disable_weights=True)
 
-        self.text_encoder = text_encoder
-        self.tokenizer = tokenizer
+    @property
+    def emphasis(self) -> "emphasis.Emphasis":
+        return emphasis.EmphasisNone()
 
-        self.id_start = 1
-        self.id_pad = 0
-        self.intermediate_output = -2
-        self.layer_norm_hidden_state = False
+    def tokenize(self, texts: list[str]) -> tuple[list[int], list[int]]:
+        if isinstance(texts, str):
+            return self.tokenizer.tokenizer(texts)["input_ids"]
+        else:
+            return [self.tokenizer.tokenizer(t)["input_ids"] for t in texts]
 
-    def tokenize(self, texts):
-        tokenized = self.tokenizer(images=None, text=texts)["input_ids"]
-        return tokenized
-
-    def encode_with_transformers(self, tokens):
-        device = memory_management.text_encoder_device()
-        tokens = tokens.to(device)
-        return self.text_encoder(input_ids=tokens)
-
-    def tokenize_line(self, line: str):
-        parsed = parsing.parse_prompt_attention(line, self.emphasis.name)
-        tokenized = self.tokenize([text for text, _ in parsed])
-
-        chunks = []
-        chunk = PromptChunk()
-
-        def next_chunk():
-            nonlocal chunk
-
-            chunks.append(chunk)
-            chunk = PromptChunk()
-
-        for tokens, (text, weight) in zip(tokenized, parsed):
-            position = 0
-            while position < len(tokens):
-                token = tokens[position]
-                chunk.tokens.append(token)
-                chunk.multipliers.append(weight)
-                position += 1
-
-        if chunk.tokens or not chunks:
-            next_chunk()
-
-        return chunks
-
-    def __call__(self, texts):
-        # https://github.com/Comfy-Org/ComfyUI/blob/v0.19.1/comfy/text_encoders/ernie.py#L14
-        self.emphasis = emphasis.EmphasisNone()
-        if any(emphasis.uses_emphasis(x) for x in texts):
-            dynamic_args.last_extra_generation_params["Emphasis"] = self.emphasis.name
+    def __call__(self, texts: list[str]) -> list[torch.Tensor]:
+        if any(emphasis.uses_emphasis(text) for text in texts):
+            dynamic_args.last_extra_generation_params["Emphasis"] = "None"
 
         zs = []
-        cache = {}
+        cache: dict[str, torch.Tensor] = {}
 
         for line in texts:
             if line in cache:
-                line_z_values = cache[line]
+                cond = cache[line]
             else:
-                chunks = self.tokenize_line(line)
-                line_z_values = []
+                chunk = self.tokenizer.tokenize_with_weights(line, disable_weights=True)
+                cond = self.text_encoder.encode_token_weights(chunk)[0]
+                cache[line] = cond
 
-                for chunk in chunks:
-                    tokens = chunk.tokens
-                    multipliers = chunk.multipliers
-
-                    z = self.process_tokens([tokens], [multipliers])[0]
-                    line_z_values.append(z)
-                cache[line] = line_z_values
-
-            zs.extend(line_z_values)
+            zs.extend(cond)
 
         return zs
-
-    def process_embeds(self, batch_tokens):
-        device = memory_management.text_encoder_device()
-
-        embeds_out = []
-        attention_masks = []
-        num_tokens = []
-
-        for tokens in batch_tokens:
-            attention_mask = []
-            tokens_temp = []
-            eos = False
-            index = 0
-
-            for t in tokens:
-                token = int(t)
-                attention_mask.append(0 if eos else 1)
-                tokens_temp += [token]
-                if not eos and token == self.id_pad:
-                    attention_mask[-1] = 0
-                    eos = True
-                index += 1
-
-            tokens_embed = torch.tensor([tokens_temp], device=device, dtype=torch.long)
-            tokens_embed = self.text_encoder.get_input_embeddings()(tokens_embed)
-
-            index = 0
-
-            embeds_out.append(tokens_embed)
-            attention_masks.append(attention_mask)
-            num_tokens.append(sum(attention_mask))
-
-        return torch.cat(embeds_out), torch.tensor(attention_masks, device=device, dtype=torch.long), num_tokens
-
-    def process_tokens(self, batch_tokens, batch_multipliers):
-        embeds, mask, count = self.process_embeds(batch_tokens)
-
-        _, z = self.text_encoder(
-            None,
-            attention_mask=mask,
-            embeds=embeds,
-            num_tokens=count,
-            intermediate_output=self.intermediate_output,
-            final_layer_norm_intermediate=self.layer_norm_hidden_state,
-        )
-
-        return z
